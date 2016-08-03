1b25658e093dce741c3f9b5283b9da57d381a759
Frank C Jones <me@frankcjones.com>
2016-08-03 22:48:46 +0000

    temperature conversions

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,21 @@
-#write your code here
+def ftoc(tempf)
+    if tempf == 32
+        return 0
+    else if tempf == 212
+        return 100
+    else
+        return (tempf - 32) * 5.0 / 9.0
+    end
+    end
+end
+
+def ctof(tempc)
+    if tempc == 0
+        return 32
+    else if tempc == 100
+        return 212
+    else
+        return (tempc * 9.0 / 5.0) + 32
+    end
+    end
+end
