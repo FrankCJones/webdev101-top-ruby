41cf65e60fe95125bbe40c786c5a9e6ae7ab1b6c
Frank C Jones <me@frankcjones.com>
2016-08-03 23:33:25 +0000

    calculator + bonuses

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,47 @@
-#write your code here
+def add(first,second)
+    first + second
+end
+
+def subtract(first,second)
+    first - second 
+end
+
+def sum(array)
+    array.inject(0){|sum,x| sum + x }
+end
+
+def multiply(array)
+    array.inject(:*)
+end
+
+def power(first,second)
+    if second == 1
+        return first
+    else if second == 0
+        return 1
+    else
+        powerproduct = first
+        while second > 1 do
+            powerproduct = powerproduct * first
+            second = second-1
+        end
+        return powerproduct
+    end
+    end
+end
+
+def factorial(number)
+    if number == 0
+        return 1
+    else if number == 1
+        return 1
+    else
+        factproduct = 1
+        while number > 1 do
+            factproduct = factproduct * number
+            number = number - 1
+        end
+        return factproduct
+    end
+    end
+end
