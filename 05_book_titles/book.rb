691f559abba9cf3aaa0e3a7b994c56882b976245
Frank C Jones <me@frankcjones.com>
2016-08-04 23:33:05 +0000

    The End of Book Titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+    def title=(bookTitle)
+        phrase = bookTitle.split(" ")
+        items = phrase.length
+        place = 1
+        phrase[0] = phrase[0].capitalize
+            while place < items
+                if phrase[place] == 'and' || phrase[place] == 'in' || phrase[place] == 'the' || phrase[place] == 'of' || phrase[place] == 'a' || phrase[place] == 'an'
+                    place = place + 1
+                else
+                    phrase[place] = phrase[place].capitalize
+                    place = place + 1
+                end
+            end
+        @title = phrase.join(" ")
+    end
+    
+    def title
+		@title
+	end
 end
