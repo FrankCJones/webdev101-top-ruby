18b3a10568a63581d87cea28d02f570cdb9a64d1
Frank C Jones <me@frankcjones.com>
2016-08-04 22:40:51 +0000

    simon says...done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
-#write your code here
+def echo(say)
+    return say
+end
+
+def shout(say)
+    return say.upcase
+end
+
+def repeat(say, times=2)
+    says = say
+    while times > 1
+        says = says + " " + say
+        times = times - 1
+    end
+    return says
+end
+
+def start_of_word(say,position)
+    return say[0...position]
+end
+
+def first_word (say)
+    return say.split(" ")[0]
+end
+
+def titleize(say)
+    says = say.split(" ")
+    items = says.length
+    place = 1
+    says[0] = says[0].capitalize
+    while place < items
+        if says[place] == 'and' || says[place] == 'over' || says[place] == 'the'
+            place = place + 1
+        else
+            says[place] = says[place].capitalize
+            place = place + 1
+        end
+    end
+    return says.join(" ")
+end
